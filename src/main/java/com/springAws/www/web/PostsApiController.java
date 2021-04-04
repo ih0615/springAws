90b05469ea3074412760f8610451826963041420
ih0615 <ih0615@naver.com>
2021-04-04 19:07:39 +0900

    20210404 service dto entity 구성 rest 형식을 통한 cru 구현 test 구현

---
+++ src/main/java/com/springAws/www/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.springAws.www.web;
+
+import com.springAws.www.service.posts.PostsService;
+import com.springAws.www.web.dto.PostsResponseDto;
+import com.springAws.www.web.dto.PostsSaveRequestDto;
+import com.springAws.www.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable("id") Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable("id") Long id){
+        return postsService.findById(id);
+    }
+}
